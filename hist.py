8b199828991ce5443633f62a9bfeb820f2b2a355
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-05 15:49:51 +0100

    added logarithmic option and plotting gn2/reco by themselves

--- hist.py
+++ hist.py
@@ -6,11 +6,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('input_file', type=str)
 parser.add_argument('output_file', type=str)
-parser.add_argument('plot_which', type=str, choices=['GN2', 'reco'])
+parser.add_argument('plot_which', type=str, choices=['GN2_minus_truth', 'reco_minus_truth', 'GN2', 'reco'])
 parser.add_argument('xmin', type=float)
 parser.add_argument('xmax', type=float)
 parser.add_argument('ymin', type=float)
 parser.add_argument('ymax', type=float)
+parser.add_argument('--logarithmic', '-l', action='store_true')
+parser.add_argument('--bins', '-b', type=int, default=100)
 
 args = parser.parse_args()
 
@@ -21,10 +23,14 @@ xmin = args.xmin
 xmax = args.xmax
 ymin = args.ymin
 ymax = args.ymax
+islog = args.logarithmic
+no_bins = args.bins
 
 label_dict = {
-    'GN2': 'GN2 Jet $p_T$ - Truth Jet $p_T$',
-    'reco': 'Jet $p_T$ - Truth Jet $p_T$'
+    'GN2_minus_truth': 'GN2 Jet $p_T$ - Truth Jet $p_T$ / GeV',
+    'reco_minus_truth': 'Reco Jet $p_T$ - Truth Jet $p_T$ / GeV',
+    'GN2': 'GN2 Jet $p_T$ / GeV',
+    'reco': 'Reco Jet $p_T$ / GeV'
 }
 
 with h5py.File(input_file, 'r') as h5file:
@@ -33,18 +39,25 @@ with h5py.File(input_file, 'r') as h5file:
 truth_pt = jets['truth_pt']
 # opt_pt = []
 
-if plot_which == 'GN2':
-    opt_pt = jets['GN2_truth_pt']
+if plot_which == 'GN2_minus_truth':
+    opt = jets['GN2_truth_pt'] - jets['truth_pt']
+elif plot_which == 'reco_minus_truth':
+    opt = jets['pt'] - jets['truth_pt']
+elif plot_which == 'GN2':
+    opt = jets['GN2_truth_pt']
 elif plot_which == 'reco':
-    opt_pt = jets['pt']
+    opt = jets['pt']
 
-opt_minus_truth = opt_pt - truth_pt
+plt.figure(figsize=(12, 10))
 
-plt.figure(figsize=(10, 10))
+if islog:
+    plt.hist2d(truth_pt, opt, bins=no_bins, range=[[xmin, xmax], [ymin, ymax]], cmap='jet', norm='log')
+else:
+    plt.hist2d(truth_pt, opt, bins=no_bins, range=[[xmin, xmax], [ymin, ymax]], cmap='jet')
 
-plt.hist2d(truth_pt, opt_minus_truth, bins=100, range=[[xmin, xmax], [ymin, ymax]], cmap='inferno')
 plt.ylabel(label_dict[plot_which])
-plt.xlabel('Truth Jet $p_T$')
+plt.xlabel('Truth Jet $p_T$ / GeV')
+plt.colorbar()
 
 plt.savefig(output_file, bbox_inches='tight')
 plt.close()
