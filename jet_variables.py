86d8c5595dd3b1ac15238960ef3dddc317442f3c
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-05 12:04:59 +0100

    added ability to plot without flavours

--- jet_variables.py
+++ jet_variables.py
@@ -11,6 +11,7 @@ parser.add_argument('parameter', type=str, choices=['truth_pt', 'pt', 'eta', 'ph
                                                     'GN2_truth_pt', 'jet_minus_truth', 'GN2_minus_truth', 'GN2_truth_rel'])
 parser.add_argument('xmin', type=float)
 parser.add_argument('xmax', type=float)
+parser.add_argument('--regression_only', '-r', action='store_true')
 
 args = parser.parse_args()
 
@@ -19,6 +20,7 @@ output_file = args.output_file
 parameter = args.parameter
 xmin = args.xmin
 xmax = args.xmax
+regression_only = args.regression_only
 
 labels_dict = {
     'truth_pt': 'Jet Truth $p_T$ / GeV',
@@ -37,39 +39,6 @@ labels_dict = {
 with h5py.File(input_file, 'r') as h5file:
     jets = h5file['jets'][:]
 
-matched_mask = jets['is_matched'] == True
-matched_jets = jets[matched_mask]
-
-q_mask = matched_jets['flavour_label'] == 0
-c_mask = matched_jets['flavour_label'] == 1
-b_mask = matched_jets['flavour_label'] == 2
-
-q_all = matched_jets[q_mask]
-c_all = matched_jets[c_mask]
-b_all = matched_jets[b_mask]
-
-if parameter == 'jet_minus_truth':
-    q_plot = q_all['pt'] - q_all['truth_pt']
-    c_plot = c_all['pt'] - c_all['truth_pt']
-    b_plot = b_all['pt'] - b_all['truth_pt']
-elif parameter == 'GN2_minus_truth':
-    q_plot = q_all['GN2_truth_pt'] - q_all['truth_pt']
-    c_plot = c_all['GN2_truth_pt'] - c_all['truth_pt']
-    b_plot = b_all['GN2_truth_pt'] - b_all['truth_pt']
-elif parameter == 'GN2_truth_rel':
-    q_plot = (q_all['GN2_truth_pt'] - q_all['truth_pt']) / q_all['truth_pt']
-    c_plot = (c_all['GN2_truth_pt'] - c_all['truth_pt']) / c_all['truth_pt']
-    b_plot = (b_all['GN2_truth_pt'] - b_all['truth_pt']) / b_all['truth_pt']
-else:
-    q_plot = q_all[parameter]
-    c_plot = c_all[parameter]
-    b_plot = b_all[parameter]
-
-h_q = Histogram(values=q_plot, flavour='ujets', bins=np.linspace(xmin, xmax, 100))
-h_c = Histogram(values=c_plot, flavour='cjets', bins=np.linspace(xmin, xmax, 100))
-h_b = Histogram(values=b_plot, flavour='bjets', bins=np.linspace(xmin, xmax, 100))
-
-
 plot = HistogramPlot(
     ylabel='Normalised number of jets',
     xlabel=labels_dict[parameter],
@@ -77,9 +46,64 @@ plot = HistogramPlot(
     atlas_brand = 'Muon Collider'
 )
 
-plot.add(h_q)
-plot.add(h_c)
-plot.add(h_b)
+matched_mask = jets['is_matched'] == True
+matched_jets = jets[matched_mask]
+
+# if only regression, don't split into flavours.
+if regression_only:
+
+    if parameter == 'jet_minus_truth':
+        all_plot = matched_jets['pt'] - matched_jets['truth_pt']
+
+    elif parameter == 'GN2_minus_truth':
+        all_plot = matched_jets['GN2_truth_pt'] - matched_jets['truth_pt']
+
+    elif parameter == 'GN2_truth_rel':
+        all_plot = (matched_jets['GN2_truth_pt'] - matched_jets['truth_pt']) / matched_jets['truth_pt']
+        
+    else:
+        all_plot = matched_jets[parameter]
+
+    h_all = Histogram(values=all_plot, bins=np.linspace(xmin, xmax, 100))
+    plot.add(h_all)
+
+else:
+
+    q_mask = matched_jets['flavour_label'] == 0
+    c_mask = matched_jets['flavour_label'] == 1
+    b_mask = matched_jets['flavour_label'] == 2
+
+    q_all = matched_jets[q_mask]
+    c_all = matched_jets[c_mask]
+    b_all = matched_jets[b_mask]
+
+    if parameter == 'jet_minus_truth':
+        q_plot = q_all['pt'] - q_all['truth_pt']
+        c_plot = c_all['pt'] - c_all['truth_pt']
+        b_plot = b_all['pt'] - b_all['truth_pt']
+
+    elif parameter == 'GN2_minus_truth':
+        q_plot = q_all['GN2_truth_pt'] - q_all['truth_pt']
+        c_plot = c_all['GN2_truth_pt'] - c_all['truth_pt']
+        b_plot = b_all['GN2_truth_pt'] - b_all['truth_pt']
+        
+    elif parameter == 'GN2_truth_rel':
+        q_plot = (q_all['GN2_truth_pt'] - q_all['truth_pt']) / q_all['truth_pt']
+        c_plot = (c_all['GN2_truth_pt'] - c_all['truth_pt']) / c_all['truth_pt']
+        b_plot = (b_all['GN2_truth_pt'] - b_all['truth_pt']) / b_all['truth_pt']
+
+    else:
+        q_plot = q_all[parameter]
+        c_plot = c_all[parameter]
+        b_plot = b_all[parameter]
+
+    h_q = Histogram(values=q_plot, flavour='ujets', bins=np.linspace(xmin, xmax, 100))
+    h_c = Histogram(values=c_plot, flavour='cjets', bins=np.linspace(xmin, xmax, 100))
+    h_b = Histogram(values=b_plot, flavour='bjets', bins=np.linspace(xmin, xmax, 100))
+
+    plot.add(h_q)
+    plot.add(h_c)
+    plot.add(h_b)
 
 plot.draw()
 plot.savefig(output_file)
