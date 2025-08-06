9b2ee67879247c6ad37b045dfe878b3245e11e58
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-06 16:09:31 +0100

    added skewing for gn2-truth and gn2

--- skew_h5.py
+++ skew_h5.py
@@ -6,19 +6,31 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('input_file', type=str)
 parser.add_argument('output_file', type=str)
+parser.add_argument('which', choices=['GN2', 'GN2_minus_truth'])
+parser.add_argument('--custom_mc', '-c', nargs=2, type=float)
 
 args = parser.parse_args()
 
 input_file = args.input_file
 output_file= args.output_file
+custom_mc = args.custom_mc
+which = args.which
 
 with h5py.File(input_file, 'r') as h5file:
     jets = h5file['jets'][:]
 
 xdata = jets['truth_pt']
-ydata = jets['GN2_truth_pt'] - jets['truth_pt']
+if which == 'GN2_minus_truth':
+    ydata = jets['GN2_truth_pt'] - jets['truth_pt']
+elif which == 'GN2':
+    ydata = jets['GN2_truth_pt']
 
-m, c = regression.linear_regression(xdata, ydata)
+if custom_mc is not None:
+    m = custom_mc[0]
+    c = custom_mc[1]
+else:
+    m, c = regression.linear_regression(xdata, ydata)  
+
 updated_gn2_truth = regression.skew_data(xdata, ydata, m, c)
 updated_gn2 = updated_gn2_truth + xdata
 
