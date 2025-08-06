deda3c7e2494f5b3cbc4963792a200d96714beca
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-06 10:51:14 +0100

    formatting

--- hist.py
+++ hist.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import h5py
 import argparse
-from . import regression
+import regression # type: ignore
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('input_file', type=str)
 parser.add_argument('output_file', type=str)
 parser.add_argument('plot_which', type=str, choices=['GN2_minus_truth', 'reco_minus_truth', 'GN2', 'reco'])
-parser.add_argument('xcoords', type=tuple)
-parser.add_argument('ycoords', type=tuple)
+parser.add_argument('coords', nargs=4, type=float)
 parser.add_argument('--logarithmic', '-l', action='store_true')
 parser.add_argument('--bins', '-b', type=int, default=100)
 parser.add_argument('--skew', '-s', action='store_true')
@@ -19,8 +18,7 @@ args = parser.parse_args()
 input_file = args.input_file
 output_file = args.output_file
 plot_which = args.plot_which
-xcoords = args.xcoords
-ycoords = args.ycoords
+coords = args.coords
 islog = args.logarithmic
 no_bins = args.bins
 skew = args.skew
@@ -39,15 +37,15 @@ plotx = jets['truth_pt']
 
 if plot_which == 'GN2_minus_truth':
     ploty = jets['GN2_truth_pt'] - jets['truth_pt']
+    m, c = regression.linear_regression(plotx, ploty)
     if skew:
-        m, c = regression.linear_regression(plotx, ploty)
-        ploty = regression.skew_data(ploty, m, c)
+        ploty = regression.skew_data(plotx, ploty, m, c)
 
 elif plot_which == 'reco_minus_truth':
     ploty = jets['pt'] - jets['truth_pt']
+    m, c = regression.linear_regression(plotx, ploty)
     if skew:
-        m, c = regression.linear_regression(plotx, ploty)
-        ploty = regression.skew_data(ploty, m, c)
+        ploty = regression.skew_data(plotx, ploty, m, c)
 
 elif plot_which == 'GN2':
     ploty = jets['GN2_truth_pt']
@@ -59,9 +57,9 @@ elif plot_which == 'reco':
 plt.figure(figsize=(12, 10))
 
 if islog:
-    plt.hist2d(plotx, ploty, bins=no_bins, range=[[xcoords[0], xcoords[1]], [ycoords[0], ycoords[1]]], cmap='jet', norm='log')
+    plt.hist2d(plotx, ploty, bins=no_bins, range=[[coords[0], coords[1]], [coords[2], coords[3]]], cmap='jet', norm='log')
 else:
-    plt.hist2d(plotx, ploty, bins=no_bins, range=[[xcoords[0], xcoords[1]], [ycoords[0], ycoords[1]]], cmap='jet')
+    plt.hist2d(plotx, ploty, bins=no_bins, range=[[coords[0], coords[1]], [coords[2], coords[3]]], cmap='jet')
 
 plt.ylabel(label_dict[plot_which])
 plt.xlabel('Truth Jet $p_T$ / GeV')
