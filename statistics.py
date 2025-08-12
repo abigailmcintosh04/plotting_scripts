9f9aaa0cfc287355dd5b521448abed775007167d
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-12 13:15:18 +0100

    added ability to calculate for jet/track vars

--- statistics.py
+++ statistics.py
@@ -5,35 +5,28 @@ import h5py
 parser = argparse.ArgumentParser()
 
 parser.add_argument('input_file', type=str)
-parser.add_argument('parameter', type=str,choices=['truth_pt', 'pt', 'eta', 'phi', 'energy', 'mass', 'dr', 
-                                                    'GN2_truth_pt', 'jet_minus_truth', 'GN2_minus_truth', 'GN2_truth_rel'])
-parser.add_argument('mean_or_median', type=str, choices=['mean', 'median'])
+parser.add_argument('jetortrack', type=str, choices=['jet', 'consts'])
+parser.add_argument('parameter', type=str)
 
 args = parser.parse_args()
 input_file = args.input_file
+jetortrack = args.jetortrack
 parameter = args.parameter
-mean_or_median = args.mean_or_median
 
 with h5py.File(input_file, 'r') as h5file:
     jets = h5file['jets'][:]
+    consts = h5file['consts'][:]
 
-if parameter == 'jet_minus_truth':
-    dataset = jets['pt'] - jets['truth_pt']
+valid_mask = consts['valid'] == True
+valid_consts = consts[valid_mask]
 
-elif parameter == 'GN2_minus_truth':
-    dataset = jets['GN2_truth_pt'] - jets['truth_pt']
-
-elif parameter == 'GN2_truth_rel':
-    dataset = (jets['GN2_truth_pt'] - jets['truth_pt']) / jets['truth_pt']
-
-else:
+if jetortrack == 'jet':
     dataset = jets[parameter]
 
-if mean_or_median == 'mean':
-    oper = np.mean(dataset)
-elif mean_or_median == 'median':
-    oper = np.median(dataset)
+elif jetortrack == 'consts':
+    dataset = valid_consts[parameter]
 
+mean = np.mean(dataset)
 std = np.std(dataset)
 
-print(f'{mean_or_median.capitalize()}: {oper}\nStandard Deviation: {std}')
+print(f'{parameter}\nMean: {mean}\nStandard Deviation: {std}')
