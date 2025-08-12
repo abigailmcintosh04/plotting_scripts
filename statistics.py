d6655d247cb963243817dd30edc12be1c28d54cb
Abigail McIntosh <abimcintosh56@gmail.com>
2025-08-12 15:00:24 +0100

    added distinction between jet and track

--- statistics.py
+++ statistics.py
@@ -13,18 +13,22 @@ input_file = args.input_file
 jetortrack = args.jetortrack
 parameter = args.parameter
 
-with h5py.File(input_file, 'r') as h5file:
-    jets = h5file['jets'][:]
-    consts = h5file['consts'][:]
+if jetortrack == 'consts':
+    with h5py.File(input_file, 'r') as h5file:
+        data = h5file['consts'][:] 
+    valid_mask = data['valid'] == True
+    data = data[valid_mask]
+    dataset = data[parameter]
 
-valid_mask = consts['valid'] == True
-valid_consts = consts[valid_mask]
-
-if jetortrack == 'jet':
-    dataset = jets[parameter]
-
-elif jetortrack == 'consts':
-    dataset = valid_consts[parameter]
+elif jetortrack == 'jet':
+    with h5py.File(input_file, 'r') as h5file:
+        jets = h5file['jets'][:]
+    if parameter == 'GN2_minus_truth':
+        dataset = jets['GN2_truth_pt'] - jets['truth_pt']
+    elif parameter == 'jet_minus_truth':
+        dataset = jets['pt'] - jets['truth_pt']
+    else:
+        dataset = jets[parameter]
 
 mean = np.mean(dataset)
 std = np.std(dataset)
